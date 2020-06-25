8fb5aaed8c6aeec1fb9b1176557595a34e6ade3c
Rayyan Khalid <rayyankhalid99@gmail.com>
2020-06-26 00:07:43 +0500

    [Data Lake] Process data in song directory

--- etl.py
+++ etl.py
@@ -18,3 +18,30 @@ def create_spark_session():
         .getOrCreate()
     return spark
 
+
+def process_song_data(spark, input_data, output_data):
+    """Process song data files and store results for songs and artists table in S3
+    Args:
+        spark (obj): spark instance
+        input_data (str): path to read data of log files from Storage
+        output_data (str): path to store tables parquet file in S3 Storage
+    """
+    # get filepath to song data file
+    song_data = input_data + "song_data/*/*/*/*.json"
+    
+    # read song data file
+    df = spark.read.json(song_data).dropDuplicates()
+
+    # extract columns to create songs table
+    songs_table = df.select(["song_id", "title", "artist_id", "year", "duration"])
+    
+    # write songs table to parquet files partitioned by year and artist
+    songs_table.write.partitionBy("year", "artist_id").parquet(output_data + "songs.parquet", mode="overwrite")
+
+    # extract columns to create artists table
+    artist_columns = ["artist_id", "artist_name as name", "artist_location as location", "artist_latitude as latitude", "artist_longitude as longitude"]
+    artists_table = df.selectExpr(*artist_columns)
+    
+    # write artists table to parquet files
+    artists_table.write.parquet(output_data + "artists.parquet", mode="overwrite")
+
